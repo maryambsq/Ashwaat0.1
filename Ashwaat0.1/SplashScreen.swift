1bdde9f6b98950e40e7387c37a99124ca9b579ba
maryambsq <maryamx399@gmail.com>
2025-05-12 10:45:20 +0300

    added splashscreen

---
+++ Ashwaat0.1/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  Ashwaat0.1
+//
+//  Created by Maryam Amer Bin Siddique on 13/11/1446 AH.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
